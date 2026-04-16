ba84dbc9223ef30997dd4fea45cc5796a582a341
TGScotGov <wisbeech@gmail.com>
2026-04-16 14:20:23 +0100

    Have added a useful thing?

--- test.ps1
+++ test.ps1
@@ -1,2 +1,8 @@
-Write-Host "Running PowerShell script..."
-Write-Host "Everything works!"
+# test.ps1
+$number = 5
+if ($number -eq 5) {
+    Write-Host "Test passed"
+} else {
+    Write-Host "Test failed"
+}
+
